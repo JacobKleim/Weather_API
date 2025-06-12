af77615caff3b07cbb5cc08c5a0cec45530ebfe6
Яков Клейменов <yakovkleimenov@yandex.ru>
2025-06-12 22:44:27 +0400

    Add reverse path

--- src/api/urls.py
+++ src/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CurrentWeatherView, ForecastWeatherView
 
 urlpatterns = [
-    path("weather/current", CurrentWeatherView.as_view()),
-    path("weather/forecast", ForecastWeatherView.as_view()),
+    path("weather/current", CurrentWeatherView.as_view(), name="current-weather"),
+    path("weather/forecast", ForecastWeatherView.as_view(), name="forecast-weather"),
 ]
